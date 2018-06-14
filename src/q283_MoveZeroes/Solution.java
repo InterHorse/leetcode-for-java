86e1a322483844c4a7b7b549532cd97a200bdd7a
MaYuzhe <514969951@qq.com>
2018-06-14 13:17:05 +0800

    添加第283题

---
+++ src/q283_MoveZeroes/Solution.java
@@ -0,0 +1,51 @@
+package q283_MoveZeroes;
+
+/**
+ * Created by IDEA
+ *
+ * @author Ma Yuzhe
+ * @date 2018/6/14
+ * @time 13:15
+ * <p>
+ * Runtime: 3ms
+ * <p>
+ * EN:
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Note:
+ * <p>
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ * <p>
+ * 中文：移动零
+ * 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [0,1,0,3,12]
+ * 输出: [1,3,12,0,0]
+ * 说明:
+ * <p>
+ * 必须在原数组上操作，不能拷贝额外的数组。
+ * 尽量减少操作次数。
+ */
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int p = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[p++] = nums[i];
+            }
+        }
+        if (p == 0) {
+            return;
+        }
+        for (int i = p; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
+}
