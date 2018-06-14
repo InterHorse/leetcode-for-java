3a36a29f0a8adc06ace78875aa15377854acbe79
MaYuzhe <514969951@qq.com>
2018-06-14 13:14:35 +0800

    添加第66题

---
+++ src/q66_PlusOne/Solution.java
@@ -0,0 +1,63 @@
+package q66_PlusOne;
+
+/**
+ * Created by IDEA
+ *
+ * @author Ma Yuzhe
+ * @date 2018/6/14
+ * @time 13:13
+ * <p>
+ * Runtime: 0ms
+ * <p>
+ * EN:
+ * Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+ * <p>
+ * The digits are stored such that the most significant digit is at the head of the list, and each element in the array contain a single digit.
+ * <p>
+ * You may assume the integer does not contain any leading zero, except the number 0 itself.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,2,3]
+ * Output: [1,2,4]
+ * Explanation: The array represents the integer 123.
+ * Example 2:
+ * <p>
+ * Input: [4,3,2,1]
+ * Output: [4,3,2,2]
+ * Explanation: The array represents the integer 4321.
+ * <p>
+ * 中文：加一
+ * 给定一个非负整数组成的非空数组，在该数的基础上加一，返回一个新的数组。
+ * <p>
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * <p>
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 示例 2:
+ * <p>
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ */
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for (int i = len - 1; i >= 0; i--) {
+            if (++digits[i] == 10) {
+                digits[i] = 0;
+
+            } else {
+                return digits;
+            }
+        }
+        int[] newArr = new int[digits.length + 1];
+        newArr[0]++;
+        return newArr;
+    }
+}
