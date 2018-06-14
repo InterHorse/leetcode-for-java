a9dc330b4f0437de959c50860ab76592ec83af39
MaYuzhe <514969951@qq.com>
2018-06-14 13:09:06 +0800

    添加第217题

---
+++ src/q217_ContainsDuplicate/Solution1.java
@@ -0,0 +1,61 @@
+package q217_ContainsDuplicate;
+
+/**
+ * Created by IDEA
+ *
+ * @author Ma Yuzhe
+ * @date 2018/6/14
+ * @time 13:05
+ * <p>
+ * Runtime: 400ms
+ * <p>
+ * 思路：两次for循环遍历比对，暴力法，复杂度为O(N²)
+ * <p>
+ * EN:
+ * Given an array of integers, find if the array contains any duplicates.
+ * <p>
+ * Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [1,2,3,1]
+ * Output: true
+ * Example 2:
+ * <p>
+ * Input: [1,2,3,4]
+ * Output: false
+ * Example 3:
+ * <p>
+ * Input: [1,1,1,3,3,4,3,2,4,2]
+ * Output: true
+ * <p>
+ * 中文：存在重复元素
+ * 给定一个整数数组，判断是否存在重复元素。
+ * <p>
+ * 如果任何值在数组中出现至少两次，函数返回 true。如果数组中每个元素都不相同，则返回 false。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: [1,2,3,1]
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: [1,2,3,4]
+ * 输出: false
+ * 示例 3:
+ * <p>
+ * 输入: [1,1,1,3,3,4,3,2,4,2]
+ * 输出: true
+ */
+public class Solution1 {
+    public boolean containsDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
