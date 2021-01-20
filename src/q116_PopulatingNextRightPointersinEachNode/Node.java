c3a04a5c9572523a150aa55ffab1616fe336ba9c
InterHorse <mayuzhe1222@qq.com>
2021-01-20 22:31:14 +0800

    feat: 添加 q116_PopulatingNextRightPointersinEachNode 填充每个节点的下一个右侧节点指针

---
+++ src/q116_PopulatingNextRightPointersinEachNode/Node.java
@@ -0,0 +1,25 @@
+package q116_PopulatingNextRightPointersinEachNode;
+
+/**
+ * @author InterHorse
+ * @date 2021-01-20 22:11:31
+ */
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
---
+++ src/q116_PopulatingNextRightPointersinEachNode/Solution.java
@@ -0,0 +1,51 @@
+package q116_PopulatingNextRightPointersinEachNode;
+
+/**
+ * @author InterHorse
+ * @date 2021-01-20 22:09:14
+ * <p>
+ * 116. 填充每个节点的下一个右侧节点指针
+ * 给定一个 完美二叉树 ，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * <p>
+ * struct Node {
+ * int val;
+ * Node *left;
+ * Node *right;
+ * Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * <p>
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ * <p>
+ * <p>
+ * 进阶：
+ * <p>
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * <p>
+ * <p>
+ * 示例：
+ * <p>
+ * <p>
+ * <p>
+ * 输入：root = [1,2,3,4,5,6,7]
+ * 输出：[1,#,2,3,#,4,5,6,7,#]
+ * 解释：给定二叉树如图 A 所示，你的函数应该填充它的每个 next 指针，以指向其下一个右侧节点，如图 B 所示。序列化的输出按层序遍历排列，同一层节点由 next 指针连接，'#' 标志着每一层的结束。
+ * <p>
+ * https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node/
+ */
+public class Solution {
+    public Node connect(Node root) {
+        if (root == null) {
+            return root;
+        }
+        if (root.left != null) {
+            root.left.next = root.right;
+            root.right.next = root.next == null ? null : root.next.left;
+            connect(root.left);
+            connect(root.right);
+        }
+        return root;
+    }
+}
